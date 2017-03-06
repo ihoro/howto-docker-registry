6ed7ad7d9c6448dbba073244e3124f10a9e5c001
phigoro <phigoro@users.noreply.github.com>
2017-03-06 21:42:57 +0200

    fix config usage

--- ubuntu/create.sh
+++ ubuntu/create.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker run -p 127.0.0.1:5000:5000 -d --restart=always -v $PWD/var-lib-registry:/var/lib/registry --name registry registry:2.6.0
+docker run -p 127.0.0.1:5000:5000 -d --restart=always -v $PWD/var-lib-registry:/var/lib/registry -v $PWD/config.yml:/etc/docker/registry/config.yml --name registry registry:2.6.0
 
